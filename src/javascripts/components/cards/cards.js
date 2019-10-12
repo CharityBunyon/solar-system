e4e58039088c5ed7e4e20318c78071161327aefd
Charity Bunyon <cseymou5@gmail.com>
2019-10-12 14:41:04 -0500

    add mouseevent

--- src/javascripts/components/cards/cards.js
+++ src/javascripts/components/cards/cards.js
@@ -1,7 +1,24 @@
+import $ from 'jquery';
 import utilities from '../../helpers/utilities';
 import './cards.scss';
 import planets from '../../helpers/data/planetData';
 
+const showHide = () => {
+  $(document).ready(() => {
+    $('.planetCard').mouseover((e) => {
+      const image = $(e.target.closest('.planetCard')).find('.back');
+      const title = $(e.target.closest('.planetCard')).find('.front');
+      title.hide();
+      image.show();
+    });
+    $('.planetCard').mouseout((e) => {
+      const image = $(e.target.closest('.planetCard')).find('.back');
+      const title = $(e.target.closest('.planetCard')).find('.front');
+      title.show();
+      image.hide();
+    });
+  });
+};
 
 const printCards = () => {
   const planetList = planets.getPlanets();
@@ -10,20 +27,17 @@ const printCards = () => {
   for (let i = 0; i < planetList.length; i += 1) {
     const planet = planetList[i];
     domString += `
-      <div class="col-4">
-      <div class="planetCard">
-      <figure class="front">
-        <h4 >${planet.name}</h4>
-      </figure>
-      <figure class="back">
-        <img src="${planet.imageUrl}" alt="Avatar" style="width: 300px; height:300px;">
-      </figure>
-    </div>
+      <div class="col-4 planetCard">
+        <h4 class="front">${planet.name}</h4>
+        <img class="planetImage back" src="${planet.imageUrl}" alt="${planet.name}" style="width: 100px; height:100px">
     </div>
     `;
   }
   domString += '</div>';
   utilities.printToDom('solarCards', domString);
+  showHide();
+  $('.planetImage').hide();
 };
+
 
 export default { printCards };
