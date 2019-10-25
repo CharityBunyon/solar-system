51e67d8d1bb68f76200540db0fc54ec59f17a230
Charity Bunyon <cseymou5@gmail.com>
2019-10-24 22:05:20 -0500

    modal kind of works

--- src/javascripts/components/modal/modal.js
+++ src/javascripts/components/modal/modal.js
@@ -3,52 +3,58 @@ import utilities from '../../helpers/utilities';
 import './modal.scss';
 import planets from '../../helpers/data/planetData';
 
-const planetList = planets.getPlanet;
-
-// eslint-disable-next-line no-unused-vars
-let currentPlanet;
 
 const soloCardPrinter = (monkeyButt) => {
-  $('#solarCards').toggleClass('d-none');
+  // $('#solarCards').toggleClass('d-none');
   const domString = `
-      <button type="button" class="closebutton btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg">Large modal</button>
-      <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg">
-          <div class="modal-content">
-            <img class="card-img-top" src="${monkeyButt.imageUrl}" alt="Image of ${monkeyButt.name}">
-            <div class="">
-              <p class="card-text">Gas Planet:${monkeyButt.isGasPlanet}</p>
-              <p class="card-text">Number of Moons:${monkeyButt.numberOfMoons}</p>
-              <p class="card-text">Name of Largest Moon:${monkeyButt.nameOfLargestMoon}</p>
-            </div>
+    <div class="modal" tabindex="-1" role="dialog">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title">${monkeyButt.name}/h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+          <img class="card-img-top" src="${monkeyButt.imageUrl}" alt="Image of ${monkeyButt.name}">
+          <div class="">
+            <p class="card-text">Gas Planet:${monkeyButt.isGasPlanet}</p>
+            <p class="card-text">Number of Moons:${monkeyButt.numberOfMoons}</p>
+            <p class="card-text">Name of Largest Moon:${monkeyButt.nameOfLargestMoon}</p>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-primary">Save changes</button>
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
           </div>
         </div>
       </div>
+    </div>
     `;
   utilities.printToDom('planetDetails', domString);
   $('#planetDetails').toggleClass('d-none');
 };
 
-const closeModalEvent = () => {
-  $('#planetCards').toggleClass('d-none');
-  $('#planetDetails').toggleClass('d-none');
-};
+// const closeModalEvent = () => {
+//   $('#planetCards').toggleClass('d-none');
+//   $('#planetDetails').toggleClass('d-none');
+// };
 
-const closeListener = () => {
-  $('.closebutton').on('click', closeModalEvent);
-};
+// const closeListener = () => {
+//   $('.closebutton').on('click', closeModalEvent);
+// };
 
 const createModalEvent = (event) => {
   const selection = event.target;
-  const planetName = $(selection).attr('id');
+  const planetName = $(selection).closest('.planetCard').attr('id');
+  const planetList = planets.getPlanets();
   for (let i = 0; i < planetList.length; i += 1) {
-    const chosenPlanet = planetList;
-    if (chosenPlanet === planetName) {
+    const chosenPlanet = planetList[i];
+    if (chosenPlanet.name === planetName) {
       soloCardPrinter(chosenPlanet);
     }
   }
-  soloCardPrinter();
-  closeListener();
+  // closeListener();
 };
 
 const cardListener = () => {
@@ -59,5 +65,4 @@ const cardListener = () => {
   }
 };
 
-
-export default { soloCardPrinter, cardListener };
+export default { cardListener };
