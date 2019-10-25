0e79f4070083173a9dcf3de72c8888e3fd94bc74
Charity Bunyon <cseymou5@gmail.com>
2019-10-25 00:12:08 -0500

    added working search bar

--- src/javascripts/components/search/search.js
+++ src/javascripts/components/search/search.js
@@ -1,1 +1,35 @@
+import $ from 'jquery';
+import utilities from '../../helpers/utilities';
+// import planets from '../../helpers/data/planetData';
+// import cards from '../cards/cards';
+import './search.scss';
 
+
+const printSearch = () => {
+  const domString = `
+  <form onsubmit='return false'>
+  <div class='form-row'>
+      <div class="form-group col-md-6 offset-md-2 col-8">
+      <input type='text' class='form-control' id='myInput' placeholder='Enter Search Term'/>
+      </div>
+  </div>
+  </form>
+`;
+  utilities.printToDom('searchBar', domString);
+};
+
+
+const search = () => {
+  $(document).ready(() => {
+    $('#myInput').keyup(() => {
+      const value = $('#myInput').val().toLowerCase();
+      $('.planetCard').hide();
+      $(`.planetCard:contains("${value}")`).closest('.planetCard').show();
+    });
+    if ($('#myInput').val() === 'Search..') {
+      $('.planetCard').show();
+    }
+  });
+};
+
+export default { printSearch, search };
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -2,12 +2,13 @@ import 'bootstrap';
 import cards from './components/cards/cards';
 import modal from './components/modal/modal';
 import closeCard from './components/closeCard/closeCard';
-// import search from './components/search/search';
+import search from './components/search/search';
 import '../styles/main.scss';
 
 const init = () => {
   cards.printCards();
-  // search.printSearchForm();
+  search.printSearch();
+  search.search();
   // modal.soloCardPrinter();
   modal.cardListener();
   closeCard.closePlanetDetails();
